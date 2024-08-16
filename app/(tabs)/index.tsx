6e6d3256385d006cc197b1f2cb107f606b7b154f
Lesya Korniychuk <korniychukLesya@ukr.net>
2024-08-16 09:25:27 +0300

    create logo

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -8,44 +8,52 @@ import { ThemedView } from '@/components/ThemedView';
 export default function HomeScreen() {
   return (
     <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
+      headerBackgroundColor={{ light: '#fff', dark: '#1D3D47' }}
       headerImage={
+          <ThemedView style={styles.titleContainer}>
         <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
+          source={require('@/assets/images/ic_ofxord_logo_black 1.svg')}
+style={styles.reactLogo}
         />
+          </ThemedView>
       }>
+        <ThemedView style={styles.titleContainer}>
+            <Image
+                source={require('@/assets/images/ic_ofxord_logo_black 1.svg')}
+                style={styles.reactLogo}
+            />
+        </ThemedView>
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome     iuiuiui!</ThemedText>
+        <ThemedText type="title">Welcome!</ThemedText>
         <HelloWave />
       </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({ ios: 'cmd + d', android: 'cmd + m' })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
+      {/*<ThemedView style={styles.stepContainer}>*/}
+      {/*  <ThemedText type="subtitle">Step 1: Try it</ThemedText>*/}
+      {/*  <ThemedText>*/}
+      {/*    Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.*/}
+      {/*    Press{' '}*/}
+      {/*    <ThemedText type="defaultSemiBold">*/}
+      {/*      {Platform.select({ ios: 'cmd + d', android: 'cmd + m' })}*/}
+      {/*    </ThemedText>{' '}*/}
+      {/*    to open developer tools.*/}
+      {/*  </ThemedText>*/}
+      {/*</ThemedView>*/}
+      {/*<ThemedView style={styles.stepContainer}>*/}
+      {/*  <ThemedText type="subtitle">Step 2: Explore</ThemedText>*/}
+      {/*  <ThemedText>*/}
+      {/*    Tap the Explore tab to learn more about what's included in this starter app.*/}
+      {/*  </ThemedText>*/}
+      {/*</ThemedView>*/}
+      {/*<ThemedView style={styles.stepContainer}>*/}
+      {/*  <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>*/}
+      {/*  <ThemedText>*/}
+      {/*    When you're ready, run{' '}*/}
+      {/*    <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}*/}
+      {/*    <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}*/}
+      {/*    <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}*/}
+      {/*    <ThemedText type="defaultSemiBold">app-example</ThemedText>.*/}
+      {/*  </ThemedText>*/}
+      {/*</ThemedView>*/}
     </ParallaxScrollView>
   );
 }
@@ -54,17 +62,19 @@ const styles = StyleSheet.create({
   titleContainer: {
     flexDirection: 'row',
     alignItems: 'center',
-    gap: 8,
+      justifyContent: 'center',
+    gap: 34,
   },
   stepContainer: {
     gap: 8,
     marginBottom: 8,
   },
   reactLogo: {
-    height: 178,
-    width: 290,
+    height: 50,
+    width: 134,
     bottom: 0,
-    left: 0,
+    left: 50,
+
     position: 'absolute',
   },
 });
